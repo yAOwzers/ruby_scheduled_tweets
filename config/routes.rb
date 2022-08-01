c2bd5a2ae49a54fe343be36ac83a21d2875c1a1c
J Neo <joel@kisi.io>
2022-08-01 22:07:47 +0200

    Validation and sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@
 Rails.application.routes.draw do
   get 'about-us', to: 'about#index', as: :about # url helper
   
+  get "sign_up", to: "registrations#new", as: :signUp
+
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
