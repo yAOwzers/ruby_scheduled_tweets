63c0983548bbb78b85dd01eeeafbd55c45d86307
J Neo <joel@kisi.io>
2022-08-07 00:35:42 +0200

    Added Login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'about-us', to: 'about#index', as: :about # url helper
   
   get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registration#create"
   
-  post "sign_up", to: "registrations#create"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy" # these are all linked to controllers
 
