826c8a8a21cbfa3a704afdd48fbb4da98464bacd
J Neo <joel@kisi.io>
2022-08-07 00:21:23 +0200

    Added Logout page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   
   post "sign_up", to: "registrations#create"
 
+  delete "logout", to: "sessions#destroy" # these are all linked to controllers
+
   root to: "main#index"
 end
