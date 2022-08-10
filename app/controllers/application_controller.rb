c6076649c1446612e0e5c0fbebf0a7a8876d12ec
J Neo <joel@kisi.io>
2022-08-10 02:11:47 +0200

    Modifed for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user # before you run any actions
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id]) # the current user if it can find one
+    end
+  end
 end
